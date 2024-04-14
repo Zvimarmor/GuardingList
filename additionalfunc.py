fa69d8724ee02895ef663cc7d4eb601375953200
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-04-14 18:40:14 +0300

    with a lot of problems but still an improvments

--- additionalfunc.py
+++ additionalfunc.py
@@ -1,38 +1,40 @@
 
-def convert_t_to_m(h,m):
-    return h*60+m
+def convert_t_to_m(t):
+    return t*60
 
-def convert_m_to_t(m):
-    h = m//60
-    m = m%60
-    return h,m
+def convert_m_to_t(t):
+    h = t//60
+    m = t%60
+    return h+(m/100)
 
-def is_time_devide_beautifully(h,m):
-    new_time = convert_t_to_m(h,m)
+def is_time_devide_beautifully(time):
+    new_time = convert_t_to_m(time)
     if new_time%30 == 0:
         return True
     return False
 
 class Guard:
-    def __init__(self, name, group, number_of_time_to_get_up):
+    def __init__(self, name, group, guard_time):
         self.name = name
         self.group = group
-        self.number_of_time_to_get_up = number_of_time_to_get_up
+        self.guard_time = guard_time
         self.time_already_woke = 0
 
 class Guard_list:
     def __init__(self):
         self.list = {}
+        self.number_of_guards = 0
 
     def add_guard(self, guard):
         self.list[guard.name] = guard.name
         self.list[guard.group] = guard.group
-        self.list[guard.number_of_time_to_get_up] = guard.number_of_time_to_get_up
+        self.list[guard.guard_time] = guard.guard_time
         self.list[guard.time_already_woke] = guard.time_already_woke
+        self.number_of_guards += 1
 
     def print_list(self):
         for name in self.list:
-            print(name, self.number_of_time_to_get_up)
+            print(name, self.guard_time)
 
 
 
---
+++ main.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from additionalfunc import *
+
+def easy_case(total_time, Guard_list):
+    total_time = convert_t_to_m(total_time)
+    
+    if Guard_list.number_of_guards == 0:
+        return "error: no guard in the list"
+    
+    if Guard_list.number_of_guards == 1:
+        for name in range:
+            return Guard_list.list[name]
+        
+    if total_time == 0:
+        return "No one is getting up"
+
+    time_for_each = total_time//Guard_list.number_of_guards
+    for name in range(Guard_list.number_of_guards):
+        Guard_list.list[name].guard_time = time_for_each
+    return Guard_list
+
+def hard_case(total_time, Guard_list):
+    total_time = convert_t_to_m(total_time[0], total_time[1])
+    
+    if Guard_list.number_of_guards == 0:
+        return "error: no guard in the list"
+    
+    if Guard_list.number_of_guards == 1:
+        for name in range(Guard_list.number_of_guards):
+            return Guard_list.list[name]
+        
+    if total_time == 0:
+        return "No one is getting up"
+    
+    time_for_each = total_time//Guard_list.number_of_guards
+    for name in range:
+        Guard_list.list[name].guard_time = time_for_each
+    return Guard_list
+
+def main(total_time, Guard_list, current_time):
+    total_time = convert_t_to_m(total_time)
+
+    if is_time_devide_beautifully(total_time):
+        easy_case(total_time, Guard_list)
+    else:
+        hard_case(total_time, Guard_list)
+    
+    for name in range(Guard_list.number_of_guards):
+        if Guard_list.list[name].guard_time > 0:
+            print(name, "is guarding now for", convert_m_to_t(Guard_list.list[name].guard_time), "minutes")
+            Guard_list.list[name].guard_time -= 1
+            Guard_list.list[name].time_already_woke += 1
+            break
+
+    print("Time to guard is over")
+    return
+
+    
+if __name__ == "__main__":
+    Guard_list = Guard_list()
+    Guard_list.add_guard(Guard("John", "Group1", 0))
+    Guard_list.add_guard(Guard("Doe", "Group1", 0))
+    Guard_list.add_guard(Guard("Jane", "Group2", 0))
+    Guard_list.add_guard(Guard("Smith", "Group2", 0))
+    Guard_list.add_guard(Guard("Jack", "Group3", 0))
+    Guard_list.add_guard(Guard("Jill", "Group3", 0))
+    current_time = datetime.now()
+
+    main(1.5, Guard_list, current_time)
+
+
