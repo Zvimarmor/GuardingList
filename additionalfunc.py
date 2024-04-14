f29c343c970c2f6d204facd66fa09a0a6f41dd68
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-04-14 17:58:59 +0300

    just playing with it

--- additionalfunc.py
+++ additionalfunc.py
@@ -7,11 +7,11 @@ def convert_m_to_t(m):
     m = m%60
     return h,m
 
-def add_person_to_list(x,Guard_list):
-    if x not in Guard_list:
-        Guard_list.append(x)
-    return Guard_list
-
+def is_time_devide_beautifully(h,m):
+    new_time = convert_t_to_m(h,m)
+    if new_time%30 == 0:
+        return True
+    return False
 
 class Guard:
     def __init__(self, name, group, number_of_time_to_get_up):
@@ -19,3 +19,22 @@ class Guard:
         self.group = group
         self.number_of_time_to_get_up = number_of_time_to_get_up
         self.time_already_woke = 0
+
+class Guard_list:
+    def __init__(self):
+        self.list = {}
+
+    def add_guard(self, guard):
+        self.list[guard.name] = guard.name
+        self.list[guard.group] = guard.group
+        self.list[guard.number_of_time_to_get_up] = guard.number_of_time_to_get_up
+        self.list[guard.time_already_woke] = guard.time_already_woke
+
+    def print_list(self):
+        for name in self.list:
+            print(name, self.number_of_time_to_get_up)
+
+
+
+
+
